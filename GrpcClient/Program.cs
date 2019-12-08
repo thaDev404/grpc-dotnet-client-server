7f0d09ce900054e272ced21a6138b45cde7f5c04
thadev404 <oluwatobiloba.olusoga@xputertechnologies.com>
2019-12-08 06:00:52 +0100

    initial commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = new HelloRequest { Name = "test" };
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001/");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(input);
+            Console.WriteLine(reply.Message);
+            Console.ReadLine();
+        }
+    }
+}
